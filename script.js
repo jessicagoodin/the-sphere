b07f5b19e6b16e7492135c199626386bf85c7f88
Jessica Goodin <66660032+jessicagoodin@users.noreply.github.com>
2023-08-03 22:25:28 -0700

    Updates to HTML, CSS. Adding JS
    Added login button w/ pop up form, and location (state) drop down menu.

---
+++ script.js
@@ -0,0 +1,34 @@
+// MODAL LOGIN (Pop up reduces when clicked outside rather than just cancel)
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// LOCATION DROP DOWN
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
